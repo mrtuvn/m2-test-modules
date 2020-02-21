dcaf0f725e017164708da2c4e60bb753f409dd91
mrtu <ladiesman9x@gmail.com>
2020-02-21 16:18:42 +0700

    Update Routing module

--- Routing/Controller/Adminhtml/Index/Index.php
+++ Routing/Controller/Adminhtml/Index/Index.php
@@ -15,8 +15,43 @@ use Magento\Framework\App\Action\HttpGetActionInterface;
  */
 class Index extends Action implements HttpGetActionInterface
 {
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Tuna_Routing::test';
+
+    /**
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return true;
+    }
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
     public function execute()
     {
-        $this->_redirect('adminhtml/*/');
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__('Test'));
+        return $resultPage;
     }
 }
---
+++ Routing/README.md
@@ -0,0 +1,6 @@
+Routing test
+
+## Usage
+In frontend go to http://yoururl/my_front_test/
+
+In backend go to http://yoururl/my-module/
